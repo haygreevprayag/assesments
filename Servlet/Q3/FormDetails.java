9da7a4b62720a300c22474738767e686aa2823c1
haygreevprayag <66084810+haygreevprayag@users.noreply.github.com>
2021-04-21 22:16:24 +0530

    Create FormDetails.java

---
+++ Servlet/Q3/FormDetails.java
@@ -0,0 +1,38 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class FormDetails extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  
+    public FormDetails() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		response.getWriter().print("<br>"+Calendar.getInstance().getTime());
+		out.println("<h3>username="+request.getParameter("uname")+"<br>password="+request.getParameter("pwd"));
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
