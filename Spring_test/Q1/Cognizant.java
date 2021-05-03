e285080c22c43ddc0de38332eb38d48995b81384
Prayag Hayagreev <66084810+haygreevprayag@users.noreply.github.com>
2021-05-03 18:44:38 +0530

    Update Cognizant.java

--- Spring_test/Q1/Cognizant.java
+++ Spring_test/Q1/Cognizant.java
@@ -1,38 +1,45 @@
 package com;
 
-public class Cognizant<Cohort> {
-public Cognizant() {
+public class Cohorts {
+	private int CohortId;
+	private String CohortName;
+	private String emailId;
+	private long contactNo;
+	public Cohorts(int CohortId,String CohortName,String emailId,long contactNo) {
 		super();
+		System.out.println("Inside cohort class");
+		this.CohortId=CohortId;
+		this.CohortName=CohortName;
+		this.emailId=emailId;
+		this.contactNo=contactNo;
 		// TODO Auto-generated constructor stub
-		System.out.println("Inside Cognizant class");
 	}
-private String EmploymentType;
-private int year;
-private Cohort cohort;
-private int EmployeeId;
-public int getEmployeeId() {
-	return EmployeeId;
-}
-public void setEmployeeId(int employeeId) {
-	EmployeeId = employeeId;
-}
-public String getEmploymentType() {
-	return EmploymentType;
-}
-public void setEmploymentType(String employmentType) {
-	EmploymentType = employmentType;
-}
-public int getYear() {
-	return year;
-}
-public void setYear(int year) {
-	this.year = year;
-}
-public Cohort getCohort() {
-	return cohort;
-}
-public void setCohort(Cohort cohort) {
-	this.cohort = cohort;
-}
+	
+	public int getCohortId() {
+		return CohortId;
+	}
+	public void setCohortId(int cohortId) {
+		CohortId = cohortId;
+	}
+	public String getCohortName() {
+		return CohortName;
+	}
+	public void setCohortName(String cohortName) {
+		CohortName = cohortName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public long getContactNo() {
+		return contactNo;
+	}
+	public void setContactNo(long contactNo) {
+		this.contactNo = contactNo;
+	}
 
+
+	
 }
