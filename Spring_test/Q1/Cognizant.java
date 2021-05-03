0e8e92347af574139c370c1c2e289c27ed291e17
Prayag Hayagreev <66084810+haygreevprayag@users.noreply.github.com>
2021-05-03 19:51:15 +0530

    Update Cognizant.java

--- Spring_test/Q1/Cognizant.java
+++ Spring_test/Q1/Cognizant.java
@@ -1,45 +1,46 @@
 package com;
 
-public class Cohorts {
-	private int CohortId;
-	private String CohortName;
-	private String emailId;
-	private long contactNo;
-	public Cohorts(int CohortId,String CohortName,String emailId,long contactNo) {
-		super();
-		System.out.println("Inside cohort class");
-		this.CohortId=CohortId;
-		this.CohortName=CohortName;
-		this.emailId=emailId;
-		this.contactNo=contactNo;
-		// TODO Auto-generated constructor stub
-	}
-	
-	public int getCohortId() {
-		return CohortId;
-	}
-	public void setCohortId(int cohortId) {
-		CohortId = cohortId;
-	}
-	public String getCohortName() {
-		return CohortName;
-	}
-	public void setCohortName(String cohortName) {
-		CohortName = cohortName;
-	}
-	public String getEmailId() {
-		return emailId;
-	}
-	public void setEmailId(String emailId) {
-		this.emailId = emailId;
-	}
-	public long getContactNo() {
-		return contactNo;
-	}
-	public void setContactNo(long contactNo) {
-		this.contactNo = contactNo;
-	}
+public class Cognizant{
+private String EmploymentType;
+private int year;
+private int contactno;
+//private Cohort cohort;
+private int EmployeeId;
+public Cognizant(String EmploymentType,int year,int contactno,int EmployeeId) {
+	super();
+	// TODO Auto-generated constructor stub
+	System.out.println("Inside Cognizant class");
+	this.EmploymentType=EmploymentType;
+	this.year=year;
+	//this.cohort=cohort;
+	this.EmployeeId=EmployeeId;
+}
+public int getEmployeeId() {
+	return EmployeeId;
+}
+public void setEmployeeId(int employeeId) {
+	EmployeeId = employeeId;
+}
+public String getEmploymentType() {
+	return EmploymentType;
+}
+public void setEmploymentType(String employmentType) {
+	EmploymentType = employmentType;
+}
+public int getYear() {
+	return year;
+}
+public void setYear(int year) {
+	this.year = year;
+}
+public int getcontactnot() {
+	return contactno;
+}
+public void setContactno(int contactno) {
+	this.contactno = contactno;
+}
 
-
-	
+void show1(){  
+    System.out.println( EmploymentType+" "+ year+ EmployeeId+ contactno);
+}  
 }
