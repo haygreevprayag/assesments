749ec182389b3db11032644026782daedc4b38f7
Prayag Hayagreev <66084810+haygreevprayag@users.noreply.github.com>
2021-05-03 19:52:12 +0530

    Update Internship.java

--- Spring_test/Q1/Internship.java
+++ Spring_test/Q1/Internship.java
@@ -1,6 +1,20 @@
 package com;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.beans.factory.BeanFactory;  
+import org.springframework.beans.factory.xml.XmlBeanFactory;  
+import org.springframework.core.io.*;  
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Internship {
-	ApplicationContext con= new ClassPathXmlApplicationContext("applicationContext.xml");
+	public static void main(String[] args) {
+	//ApplicationContext con= new ClassPathXmlApplicationContext("applicationContext.xml");
+	 Resource r=new ClassPathResource("applicationContext.xml");  
+	  BeanFactory factory=new XmlBeanFactory(r); 
+	Cohorts cohortobj = (Cohorts) factory.getBean("one");
+	cohortobj.show();
+	Cognizant cognizantobj = (Cognizant) factory.getBean("two");
+	cognizantobj.show1();
+	//how to print all parameters ?
+	
+	
+}
 }
